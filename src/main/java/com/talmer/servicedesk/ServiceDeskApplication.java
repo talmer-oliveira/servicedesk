1904949a81e5c3ff673d80e8f41fdf03cc3d130f
Talmer Oliveira <talmer.eng@outlook.com>
2021-02-08 16:32:04 -0300

    Projeto criado

---
+++ src/main/java/com/talmer/servicedesk/ServiceDeskApplication.java
@@ -0,0 +1,13 @@
+package com.talmer.servicedesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDeskApplication.class, args);
+	}
+
+}
